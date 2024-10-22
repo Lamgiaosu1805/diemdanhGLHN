5203fe46007d7aa6ce735795aad7000b60904490
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-10-22 10:35:19 +0700

    update comment

--- src/models/SheetDiemDanhModel.js
+++ src/models/SheetDiemDanhModel.js
@@ -4,7 +4,7 @@ const moment = require('moment-timezone')
 
 const SheetDiemDanh = new Schema({
     status: { type: Number, default: 0 }, // Trạng thái chốt điểm danh: 1: Chốt mềm, 2: Chốt cứng, 0: Chưa chốt
-    time: {type: Number, required: true}, // Buổi sáng hoặc chiều: 1: Sáng, 2: Chiều 
+    time: {type: Number, required: true}, // Buổi sáng hoặc chiều: 1: Sáng, 2: Chiều , 3: Lễ
     createdAt: {
         type: String,
         default: () => moment.tz(Date.now(), 'Asia/Ho_Chi_Minh').format(), // Tự động lưu với múi giờ +7
